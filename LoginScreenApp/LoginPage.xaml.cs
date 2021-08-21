c8432591aa8e16353ff385fde751c731d7b3d418
Alexis Novas <alexismoises@hotmail.com>
2021-08-20 23:38:16 -0400

    Initial Commit

---
+++ LoginScreenApp/LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginScreenApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();  
+        }
+
+        async private void ButtonClicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFieldUsername.Text) && string.IsNullOrEmpty(txtFieldPassword.Text))
+            {
+                await DisplayAlert("Error", "Los campos están vacíos", "Ok");
+            }
+            else if (string.IsNullOrEmpty(txtFieldUsername.Text))
+            {
+                await DisplayAlert("Error", "El campo email no puede estar vacío", "Ok");
+            }
+            else if (string.IsNullOrEmpty(txtFieldPassword.Text))
+            {
+                await DisplayAlert("Error", "El campo password no puede estar vacío", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Bienvenido", $"¡Hola {txtFieldUsername.Text}!", "Ok");
+            }
+        }
+
+        async private void TapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            await DisplayAlert("Register", "This simulates Register Page", "Ok");
+        }
+    }
+}
